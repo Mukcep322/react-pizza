7701f61e43c03ae4042e6c5c11282e0783bb12ae
Mukcep322 <antongolosov48@gmail.com>
2025-07-23 12:31:23 +0300

    add components

--- react-pizza/src/App.js
+++ react-pizza/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+// Libraries
+import React from "react"
 
+// Components
+import Header from "./components/Header.jsx"
+import Categories from "./components/Categories.jsx"
+import Sort from "./components/Sort.jsx"
+import PizzaItem from "./components/PizzaItem.jsx"
+
+// Styles
+import "./scss/app.scss"
+
+// App
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="wrapper">
+      <Header />
+      <div class="content">
+        <div class="container">
+          <div class="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 class="content__title">Все пиццы</h2>
+          <div class="content__items">
+            <PizzaItem />
+            <PizzaItem />
+            <PizzaItem />
+            <PizzaItem />
+            <PizzaItem />
+            <PizzaItem />
+            <PizzaItem />
+            <PizzaItem />
+            <PizzaItem />
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
